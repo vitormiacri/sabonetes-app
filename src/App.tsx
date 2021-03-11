c2fb96e5250f1323ab327d588009d7435254232d
Vitor Morelli <vitao.miacri@gmail.com>
2021-03-11 11:10:27 -0300

    feat: add onesginal implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import OneSignal from 'react-native-onesignal';
 
 import { MainRoutes } from './routes';
 
 const App: React.FC = () => {
   useEffect(() => {
     SplashScreen.hide();
+  }, []);
+
+  useEffect(() => {
+    OneSignal.setAppId('86f85145-ca1d-4464-8efa-4076daf57a20');
+    OneSignal.setNotificationOpenedHandler(data =>
+      console.log(data.notification),
+    );
+
+    return () => {
+      OneSignal.clearHandlers();
+    };
   }, []);
 
   return (
