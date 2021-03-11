a521c74cf6efc4061e637e3f2f54ed4c4de814b8
Vitor Morelli <vitao.miacri@gmail.com>
2021-03-11 11:09:07 -0300

    feat: add details page

---
+++ src/pages/details/index.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/core';
+
+import {
+  Container,
+  CardList,
+  ImageContainer,
+  ImageCover,
+  TextContainer,
+  Title,
+  DescriptionContainer,
+  Description,
+} from './styles';
+import { ProductProps } from 'src/services/firebase-cloud-firestore';
+
+type RouteParams = {
+  product: ProductProps;
+};
+
+export const Details: React.FC<RouteParams> = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const { product } = route.params as RouteParams;
+  const [description, setDescription] = useState<string[]>(
+    product.description.split('\\r'),
+  );
+
+  useEffect(() => {
+    navigation.setOptions({ title: product.title });
+  }, [navigation]);
+
+  return (
+    <ScrollView>
+      <Container>
+        <CardList>
+          <ImageContainer>
+            <ImageCover
+              source={{ uri: product.photoUrl }}
+              resizeMode="cover"
+            ></ImageCover>
+          </ImageContainer>
+          <TextContainer>
+            <Title>{product.title}</Title>
+          </TextContainer>
+        </CardList>
+        <DescriptionContainer>
+          {description.map((item, index) => (
+            <Description key={index}>{item}</Description>
+          ))}
+        </DescriptionContainer>
+      </Container>
+    </ScrollView>
+  );
+};
---
+++ src/pages/details/styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  padding: 20px;
+`;
+
+export const CardList = styled.View`
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border: 1px solid #c4c4c4;
+`;
+
+export const ImageContainer = styled.View`
+  width: 100%;
+`;
+
+export const ImageCover = styled.Image`
+  width: 100%;
+  height: 250px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`;
+
+export const TextContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 15px;
+`;
+
+export const Title = styled.Text`
+  color: #ff92b9;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const DescriptionContainer = styled.View`
+  width: 100%;
+  border: 1px solid #c4c4c4;
+  border-top-width: 0;
+  padding: 20px 0;
+`;
+
+export const Description = styled.Text`
+  color: #000;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 5px 20px;
+`;
